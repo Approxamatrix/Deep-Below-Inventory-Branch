b4e6a34bfa1efc9c4766caa1b94a27ddcc85fe78
SilverFields <ithielnascimento@gmail.com>
2026-02-23 12:29:21 -0400

    Added Basic Health Bar
    Added a simple health bar with functions for:
    Adding Health (Healing)
    Removing Health (Receiving Damage)
    Setting a value for the health manually (Could be used for Debugging or some specific cutscene or smt)
    
    Added Signals that are emitted when:
    Player takes damage
    Player dies
    
    Adding parameter for changing max health, default is 100
    
    Removed Proxy (was just a thing I had added for fun a while back don't worry about it)

---
+++ deep-below/Scripts/Player/PlayerSystems/health_bar.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var max_health = 100.0
+@onready var progress_bar = $ProgressBar
+
+signal damage_taken(amount)
+signal died
+
+func _ready():
+	progress_bar.max_value = max_health
+	progress_bar.value = max_health
+
+func damage(amount):
+	var difference = progress_bar.value - amount
+	if !difference <= 0:
+		progress_bar.value -= amount
+		damage_taken.emit(amount)
+	elif  difference <= 0:
+		died.emit()
+
+func heal(amount):
+	progress_bar.value += amount
+
+func manual_health_set(value):
+	progress_bar.value = value
